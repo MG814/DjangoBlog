f1b38eb2c1daecf7eb8c7a9b1ca16658649de000
MICHAŁ <mg814@wp.pl>
2022-06-10 20:12:05 +0200

    CSS + szablon bazowy + szablon detail

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from .models import Post
 
 
 # funkcja post_list pobiera (czyli request) oraz zwraca (czyli return)
 # to co wyrenderuje (złoży w całoiść) szablon html wbudowana funkcja render
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(publish_date__lte=timezone.now()).order_by('publish_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
